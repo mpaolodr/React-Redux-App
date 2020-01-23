096469d63b1d6b5cd47c6fbf084a2d8f523f31cd
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-23 15:13:39 -0500

    created actions, reducers, components folder and setup store in index.js

---
+++ react-redux/src/reducers/gitReducer.js
@@ -0,0 +1,18 @@
+// import actions
+
+// initialState
+const initialState = {
+  isLoading: false,
+  user: {},
+  error: {}
+};
+
+
+export const gitReducer = (state = initialState, action) {
+    switch(action.type) {
+
+        // default case
+        default: return state;
+        
+    }
+}
---
+++ react-redux/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+// reducers
+import { gitReducer } from "./gitReducer";
+
+export default combineReducers({
+  gitReducer
+});
